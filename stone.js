dfc3d9fedce7d4839bbbe823d76b144ee212718a
ParthZemse <77433717+ParthZemse@users.noreply.github.com>
2021-04-22 15:32:51 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,44 @@
+class Stone {
+
+    constructor(x, y, width, height) {
+        var options = {
+
+            density: 12,
+            friction: 0.9,
+            restitution: 0.8
+        }
+
+
+        this.body = Bodies.rectangle(x, y, width, height, options)
+        this.width = width
+        this.height = height
+        World.add(world, this.body)
+    }
+    display() {
+
+
+
+        var pos = this.body.position
+        var angle = this.body.angle
+        pos.x = mouseX;
+        pos.y = mouseY;
+        push()
+        translate(pos.x, pos.y)
+        rotate(angle)
+        strokeWeight(4)
+        stroke('black')
+        rectMode(CENTER)
+        fill("black")
+        rect(0, 0, this.width, this.height)
+        pop()
+
+
+
+
+    }
+
+
+
+
+
+}
